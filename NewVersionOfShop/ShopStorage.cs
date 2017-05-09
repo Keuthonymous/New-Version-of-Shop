f317200819c4b8ffc2dd38a6588203dade46f671
Keuthonymous <mikie.daughtrey@gmail.com>
2017-05-09 15:58:50 +0200

    Added ShopStorage class

---
+++ NewVersionOfShop/ShopStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewVersionOfShop
+{
+    class ShopStorage : ItemStorage<Item>
+    {
+        public void ChangePriceBulk(string artNum, double price)
+        {
+            var query = from item in InternalStorage
+                        where item.ArtNum.Count() >= 1
+                        select item;
+
+        }
+    }
+}
